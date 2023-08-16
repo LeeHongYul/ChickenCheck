62becdde40334ee4823f6b238676ca0aaa307dd1
hongryul <ryullee0911@gmail.com>
2023-08-16 12:59:05 +0900

    Init Setting

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChickenCheck
+//
+//  Created by 이홍렬 on 2023/08/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
